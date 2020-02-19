b8cf74cdfe6d2b09b848acdabd85ab493052a78a
Teslim Abdulafeez <teslimafeez@gmail.com>
2020-02-19 11:40:42 +0100

    delete redundant files

--- app/containers/OrgPage/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectOrgPageDomain } from '../selectors';
-
-describe('selectOrgPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
